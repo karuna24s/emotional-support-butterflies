56a3958ec8ed8b1229f1a70c74ce9ff2e16b8a42
Katharine Francis <katie-francis@hotmail.com>
2020-03-30 16:00:48 -0700

    postgres extra setup

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  res.json({ info: "Node.js, Express, and Postgres API" });
   res.send({ response: "I am alive" }).status(200);
 });
 
