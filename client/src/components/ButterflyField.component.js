7a65bb2f4e4b143e3641478156b013432832163c
Katharine Francis <katie-francis@hotmail.com>
2020-04-02 14:30:38 -0700

    Sockets are emmiting and receiving data objects

---
+++ client/src/components/ButterflyField.component.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const ButterflyField = ({ pathName }) => {
+  console.log("ButterflyField props: ", pathName);
+  return <div className="sky"></div>;
+};
+
+export default ButterflyField;
