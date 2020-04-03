1ff0299cdc3f054de49f2dfb9106d652b2ec2d67
genvalen <genvalen@protonmail.com>
2020-04-02 20:17:12 -0400

    first commit

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-const DarkSkyApiSecret = require("./keys").DarkSkyKey;
+// const DarkSkyApiSecret = require("./keys").DarkSkyKey;
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
@@ -25,17 +25,17 @@ const server = http.createServer(app);
 
 const io = socketIo(server); // < Interesting!
 
-const getApiAndEmit = async socket => {
-  try {
-    //this is where we will make the get call to the api server (deploy an api server) to retrieve the latest butterflies (WHERE timestamp === Date.now()???)
-    const res = await axios.get(
-      `https://api.darksky.net/forecast/${DarkSkyApiSecret}/37.780206,-122.450946`
-    ); // Getting the data from DarkSky
-    socket.emit("FromAPI", res.data.currently.temperature); // Emitting a new message. It will be consumed by the client
-  } catch (error) {
-    console.error(`Error: ${error.code}`);
-  }
-};
+// const getApiAndEmit = async socket => {
+//   try {
+//     //this is where we will make the get call to the api server (deploy an api server) to retrieve the latest butterflies (WHERE timestamp === Date.now()???)
+//     const res = await axios.get(
+//       `https://api.darksky.net/forecast/${DarkSkyApiSecret}/37.780206,-122.450946`
+//     ); // Getting the data from DarkSky
+//     socket.emit("FromAPI", res.data.currently.temperature); // Emitting a new message. It will be consumed by the client
+//   } catch (error) {
+//     console.error(`Error: ${error.code}`);
+//   }
+// };
 
 let interval;
 
