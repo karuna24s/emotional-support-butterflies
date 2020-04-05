dd59b11e9bc2902fcc6f521bfc7c73c0726b2f9b
Katharine Francis <katie-francis@hotmail.com>
2020-04-04 18:41:40 -0700

    Mapping arrays to get images

--- client/src/components/ButterflyField/ButterflyField.component.js
+++ client/src/components/ButterflyField/ButterflyField.component.js
@@ -2,6 +2,19 @@ import React, { useState, useEffect } from "react";
 //import Form from "../Form/Form.component";
 import socketIOClient from "socket.io-client";
 import styles from "./ButterflyField.module.css";
+import BlueButterfly from "../../static/BlueButterfly.png";
+import OrangeButterfly from "../../static/OrangeButterfly.png";
+import PinkButterfly from "../../static/PinkButterfly.png";
+import PurpleButterfly from "../../static/PurpleButterfly.png";
+import YellowButterfly from "../../static/YellowButterfly.png";
+
+const imgObj = {
+  BlueButterfly,
+  OrangeButterfly,
+  PinkButterfly,
+  PurpleButterfly,
+  YellowButterfly,
+};
 
 import { imageData } from "../../data/imageData";
 
@@ -9,44 +22,46 @@ const url = "https://butterfly-server-api.herokuapp.com";
 const socket = socketIOClient(url);
 
 const ButterflyField = () => {
-  const [pathname, setPathName] = useState("BlueButterfly");
-  const [butterflies, setButterflies] = useState(["BlueButterfly"]);
+  //const [pathname, setPathName] = useState("BlueButterfly");
+  const [butterflies, setButterflies] = useState([]);
 
-  const [visible, setVisible] = useState(false);
+  //const [visible, setVisible] = useState(false);
 
-  function hideMe() {
-    setVisible(true);
-  }
+  // function hideMe() {
+  //   setVisible(true);
+  // }
 
   socket.on("butterfly", (data) => {
     //data given to us from the server from the server
     //console.log("Client Listening For Server: ", data);
 
-    setPathName(data.pathName);
-    setButterflies(...butterflies, data.pathName);
-    hideMe();
+    //setPathName(data.pathName);
+    setButterflies([...butterflies, data.pathName]);
+    //hideMe();
   });
-  let style = {};
-  if (!visible) style.display = "none";
+  // let style = {};
+  // if (!visible) style.display = "none";
   //butterflies[butterflies.length - 1] i.e. BlueButterfly
-  useEffect(() => {
-    console.log("Pathname: ", pathname);
-    console.log("Butterfly: ", imageData[pathname]);
-    const imgArray = imageData[pathname];
+  // useEffect(() => {
+  //   console.log("Pathname: ", pathname);
+  //   console.log("Butterfly: ", imageData[pathname]);
+  //   const imgArray = imageData[pathname];
 
-    const [img, className] = imgArray;
-    const newButterfly = document.getElementById(`#${pathname}`);
-    console.log("Clicked butterfly: ", newButterfly);
-    const newImg = document.createElement("img");
-    newImg.setAttribute("src", img);
-    //newImg.setAttribute("className", className);
-    console.log("New Image: ", newImg);
-    newButterfly.appendChild(newImg);
-  });
+  //   const [img, className] = imgArray;
+  //   const newButterfly = document.getElementById(`#${pathname}`);
+  //   console.log("Clicked butterfly: ", newButterfly);
+  //   const newImg = document.createElement("img");
+  //   newImg.setAttribute("src", img);
+  //   //newImg.setAttribute("className", className);
+  //   console.log("New Image: ", newImg);
+  //   newButterfly.appendChild(newImg);
+  // });
 
   return (
     <div className={styles.sky}>
-      <div id={pathname}></div>
+      {butterflies.map((butterfly) => (
+        <div>{butterfly}</div>
+      ))}
     </div>
   );
 };
